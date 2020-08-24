b1fab12882c92e7f9d9d378227f2ef00a7eda589
Jiuyang liu <liujiuyang1994@gmail.com>
2020-08-24 05:39:23 +0000

    update BSCANE2 docs from ug570.

--- xilinx/src/primitive/config/BSCANE2.scala
+++ xilinx/src/primitive/config/BSCANE2.scala
@@ -3,41 +3,49 @@ package xilinx.primitive.config
 import chisel3._
 import chisel3.experimental._
 
-/** This design element allows access to and from internal logic by the JTAG Boundary Scan logic
-  * controller. This allows for communication between the internal running design and the dedicated
-  * JTAG pins of the FPGA. Each instance of this design element will handle one JTAG USER
-  * instruction (USER1 through USER4) as set with the [[JTAG_CHAIN]] attribute.
+/** The BSCANE2 primitive allows access between the internal FPGA logic and the JTAG
+  * boundary scan logic controller. This allows for communication between the internal running
+  * design and the dedicated JTAG test access port (TAP) pins of the FPGA. The [[BSCANE2]]
+  * primitive must be instantiated to gain internal access to the JTAG pins. The BSCANE2
+  * primitive is not needed for normal JTAG operations that use direct access from the JTAG
+  * pins to the TAP controller. The [[BSCANE2]] is automatically added to a design when using the
+  * Vivado Logic Analyzer, or when using indirect flash programming in the Vivado Device
+  * Programmer.
   *
-  * To handle all four USER instructions, instantiate four of these elements, and set the JTAG_CHAIN
-  * attribute appropriately.
-  *
-  * For specific information on boundary scan for an architecture, see the Configuration User Guide
-  * for the specific device.
+  * 7 series FPGAs and UltraScale of BSCANE2 primitive are identical.
   *
   * @param JTAG_CHAIN Value for USER command.
-  **/
+  */
 class BSCANE2(JTAG_CHAIN: Double = 1) extends ExtModule(Map("JTAG_CHAIN" -> JTAG_CHAIN)) {
   require(1 to 4 contains JTAG_CHAIN)
-  /** [[CAPTURE]] output from TAP controller. */
+  /** Asserted when TAP controller is in Capture-DR state */
   val CAPTURE = IO(Output(Bool()))
   /** Gated [[TCK]] output. When [[SEL]] is asserted, [[DRCK]] toggles when [[CAPTURE]] or [[SHIFT]] are asserted. */
   val DRCK = IO(Output(Bool()))
-  /** Reset output for TAP controller. */
+  /** Asserted when TAP controller is in Test-Logic-Reset state. */
   val RESET = IO(Output(Bool()))
-  /** Output asserted when TAP controller is in Run Test/Idle state. */
+  /** Asserted when TAP controller is in Run-Test/Idle state. */
   val RUNTEST = IO(Output(Bool()))
-  /** USER instruction active output. */
+  /** Asserted when the USER instruction (USER1 – USER4) that corresponds to
+    * the BSCANE2 instance's JTAG_CHAIN attribute (1–4) is loaded as
+    * the active instruction in the JTAG Instruction register.
+    */
   val SEL = IO(Output(Bool()))
-  /** [[SHIFT]] output from TAP controller. */
+  /** Asserted when TAP controller is in Shift-DR state. */
   val SHIFT = IO(Output(Bool()))
-  /** Test Clock output. Fabric connection to TAP Clock pin. */
+  /** Test Clock. Primitive output from external TAP pin to FPGA internal logic. */
   val TCK = IO(Output(Bool()))
-  /** Test Data Input ([[TDI]]) output from TAP controller. */
+  /** Test Data Input. Primitive output from external TAP pin to FPGA internal logic. */
   val TDI = IO(Output(Bool()))
-  /** Test Data Output ([[TDO]]) input for USER function. */
+  /** Test Data Output. Primitive input from User internal scan register to
+    * a flip-flop that registers the primitive input on the falling edge of [[TCK]].
+    * The output of the flip-flop is forwarded to the external TAP TDO pin.
+    */
   val TDO = IO(Input(Bool()))
-  /** Test Mode Select output. Fabric connection to TAP. */
+  /** Test Mode Select. Primitive output from external TAP pin to FPGA internal logic. */
   val TMS = IO(Output(Bool()))
-  /** [[UPDATE]] output from TAP controller. */
+  /** Asserted when TAP controller is in Update state. Internal logic should update from
+    * the internal scan register on the rising edge of the [[UPDATE]] signal.
+    */
   val UPDATE = IO(Output(Bool()))
 }
