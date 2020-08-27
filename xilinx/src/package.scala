2919844cec29d2533788caa89f58e39c587d6e66
Jiuyang liu <liujiuyang1994@gmail.com>
2020-08-27 09:43:20 +0000

    add markDebug attribute annotation.

--- xilinx/src/package.scala
+++ xilinx/src/package.scala
@@ -1,5 +1,17 @@
+import chisel3.experimental.ChiselAnnotation
+import firrtl.annotations.Annotation
+
 package object xilinx {
   implicit class XilinxBoolean(param: Boolean) {
     def str = if (param) "TRUE" else "FALSE"
+  }
+  def markDebug(data: chisel3.Data) = {
+    chisel3.experimental.requireIsHardware(data)
+    chisel3.experimental.annotate(
+      new ChiselAnnotation {
+        override def toFirrtl: Annotation = firrtl.AttributeAnnotation(data.toTarget, """mark_debug = "yes"""")
+      }
+    )
+    data.toTarget
   }
 }
